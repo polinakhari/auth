79bb213780d59045bb55be6c33f4515295e78f93
polinakhari <polina14.05@yandex.ru>
2020-06-19 14:34:56 +0300

    react setup

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    login: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = User = mongoose.model('user', UserSchema) 
