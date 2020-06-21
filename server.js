70222964f8d701d7aede35be06a81c5a75ef5d9a
polinakhari <polina14.05@yandex.ru>
2020-06-21 16:21:12 +0300

    get

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
+//app.get("/", (req, res) => res.send("API Running"));
 
 //Define routes
 app.use("/api/users", require("./routes/api/users"));
